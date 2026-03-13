8724f87d41e4e250058931da55de48ea64ec2416
sharonmcg76 <106909629+sharonmcg76@users.noreply.github.com>
2026-03-13 10:03:09 +0000

    lambda practice with stream

---
+++ src/main/java/org/example/LambdaExpressions.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaExpressions {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(54);
+        list.add(43);
+        list.add(99);
+        list.add(234);
+        list.add(9);
+        list.add(12);
+        System.out.println(list);
+        List<Integer> doubleNumbers = list.stream().map(number -> number*2).toList();
+        System.out.println(doubleNumbers);
+    }
+
+}
