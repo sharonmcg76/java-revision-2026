130177f6d48cc998fea7ea43b83da1af00347fe1
sharonmcg76 <106909629+sharonmcg76@users.noreply.github.com>
2026-04-01 11:19:15 +0100

    added notes from lecturer to mock exam questions, working on solutions given.

--- src/main/java/org/example/mockexam/questiontwo/MaxCounter.java
+++ src/main/java/org/example/mockexam/questiontwo/MaxCounter.java
@@ -1,32 +1,7 @@
 package org.example.mockexam.questiontwo;
 
-public class MaxCounter extends Counter {
-    private int maxCount = 0;
-
-   @Override
-    public void reset() {
-        if (getCount() > maxCount) { //getter method as private
-            maxCount = getCount();  //update maxCount if getCount() is greater than maxCount
-        }
-        super.reset(); //calling from counter class
-    }
-    @Override  // need to change maxCount, not just count
-    public void click() {
-        super.click();
-        if (getCount() > maxCount) { //getter method as private
-            maxCount++;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "the count is " + getCount() + " and the maximum was " + maxCount;
-    }
-}
-
-
-//Fragmetn is supposed to have the output below
-// A questiontwo.MaxCounter stores the largest number of clicks before the counter is reset,
+//Fragment is supposed to have the output below
+//MaxCounter stores the largest number of clicks before the counter is reset,
 //write any additional relevant methods and override the methods for which this is necessary (and no others)
 //use the annotation @Override for all methods where this is possible.
 //output should be
@@ -36,3 +11,48 @@ public class MaxCounter extends Counter {
 //the count is 0 and the maximum was 2
 //the count is 1 and the maximum was 2
 //the count id 3 and the maximum was 3
+
+public class MaxCounter extends Counter {
+    private int maxCount = 0;
+// do NOT add a repeated instance variable count in MaxCounter; it is already
+//   present in Counter??
+
+   @Override
+    public void reset() {
+        if (getCount() > maxCount) { //getter method as private
+            maxCount = getCount();  //update maxCount if getCount() is greater than maxCount
+        }
+        super.reset(); //calling from counter class
+
+//Check with Kristoff?? Carsten said ??
+// - do NOT write a method like
+//
+//       public void reset() {
+//           super.reset();
+//       }
+//
+//   it does not add anything over the inherited method, except confusion on
+//   the side of the programmer who sees the overridden method???
+
+
+   }
+    @Override  // need to change maxCount, not just count
+    public void click() {
+        super.click();
+        //super.click() must be used to increment count since count is private
+        //   (and the responsibility of the superclass)
+        if (getCount() > maxCount) { //getter method as private
+            maxCount++;  // maxCount = getCount();??
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "the count is " + getCount() + " and the maximum was " + maxCount;
+    }
+    //return super.toString() + " and the maximum was " + maxCount;
+    // - toString() could be implemented using getCount() as well
+}
+
+
+
